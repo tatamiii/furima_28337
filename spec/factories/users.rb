eb041fe3dbe78883eb16851056ba2adc7dbb3e2e
tatamiii <sousuke2828@gmail.com>
2020-08-14 15:53:15 +0900

    テストコードを記述するファイルの用意

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {Gimei.last.kanji}
-    first_name            {Gimei.first.kanji}
-    last_name_kana        {Gimei.last.katakana}
-    first_name_kana       {Gimei.first.katakana}
-    birth_date            {{1=>1936, 2=>6, 3=>9}}
+    
   end
 end
