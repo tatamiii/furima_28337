cfcd36ab85ea777a3b68b32866503055e9e7f99d
tatamiii <sousuke2828@gmail.com>
2020-08-14 11:41:27 +0900

    FactoryBotの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,37 +4,37 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,
-  presence: true
+  # validates :nickname,
+  # presence: true
   
-  validates :email,
-  presence: true,
-  uniqueness: true,
-  inclusion: { in: %w(@) }
+  # validates :email,
+  # presence: true,
+  # uniqueness: true,
+  # inclusion: { in: %w(@) }
 
-  validates :password,
-  presence: true,
-  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
-  length: { minimum: 6 }
+  # validates :password,
+  # presence: true,
+  # format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
+  # length: { minimum: 6 }
 
-  validates :first_name,
-  presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  # validates :first_name,
+  # presence: true,
+  # format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   
-  validates :last_name,
-  presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  # validates :last_name,
+  # presence: true,
+  # format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   
-  validates :first_name_kana,
-  presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
+  # validates :first_name_kana,
+  # presence: true,
+  # format: { with: /\A[ァ-ヶー－]+\z/ }
   
-  validates :last_name_kana,
-  presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
+  # validates :last_name_kana,
+  # presence: true,
+  # format: { with: /\A[ァ-ヶー－]+\z/ }
   
-  validates :birth_date,
-  presence: true
+  # validates :birth_date,
+  # presence: true
      
      
      
