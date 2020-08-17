dab73998b3f151a0458e06ddc8a329e7d765296c
tatamiii <sousuke2828@gmail.com>
2020-08-17 12:20:51 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,38 +5,35 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,
-  presence: true
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+            presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email,
-  presence: true,
-  uniqueness: true
+            presence: true,
+            uniqueness: true
   # format: { with: VALID_EMAIL_REGEX }
 
   validates :password,
-  presence: true,
-  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
-  length: { minimum: 6 }
+            presence: true,
+            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
+            length: { minimum: 6 }
 
   validates :first_name,
-  presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  
+            presence: true,
+            format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
   validates :last_name,
-  presence: true,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  
+            presence: true,
+            format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
   validates :first_name_kana,
-  presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
-  
+            presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/ }
+
   validates :last_name_kana,
-  presence: true,
-  format: { with: /\A[ァ-ヶー－]+\z/ }
-  
+            presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/ }
+
   validates :birth_date,
-  presence: true
-     
-     
-     
+            presence: true
 end
