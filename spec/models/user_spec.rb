b728cf5061ae5b2529ea596cd1ac5d97226a7d1d
tatamiii <sousuke2828@gmail.com>
2020-08-17 12:19:58 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,8 +32,7 @@ RSpec.describe @User, type: :model do
     it "メールアドレスは@を含む必要があること" do
       @user.email = 'soumagmail.com'
       @user.valid?
-      binding.pry
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+      expect(@user.errors.full_messages).to include("Email is invalid")
     end
 
     it "パスワードが必須であること" do
