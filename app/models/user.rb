195412d80c2091b5b1093fbe0d4e857f48a37330
tatamiii <sousuke2828@gmail.com>
2020-08-18 10:30:41 +0900

    バリデーションの修正 と　エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,31 +3,39 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   with_options presence: true do
 
-  validates :nickname
+    # validates :nickname,
+    #           presence: true
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :email,
-            uniqueness: true
-  # format: { with: VALID_EMAIL_REGEX }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email,
+              # presence: true,
+              uniqueness: true
+    # format: { with: VALID_EMAIL_REGEX }
 
-  validates :password,
-            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
-            length: { minimum: 6 }
+    validates :password,
+              # presence: true,
+              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
+              length: { minimum: 6 }
 
-  validates :first_name,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name,
+              # presence: true,
+              format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
-  validates :last_name,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name,
+              # presence: true,
+              format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
-  validates :first_name_kana,
-            format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana,
+              # presence: true,
+              format: { with: /\A[ァ-ヶー－]+\z/ }
 
-  validates :last_name_kana,
-            format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana,
+              # presence: true,
+              format: { with: /\A[ァ-ヶー－]+\z/ }
 
-  validates :birth_date,
+    # validates :birth_date,
+    #           presence: true
+    end
 end
