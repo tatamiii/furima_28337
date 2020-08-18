c095592040d6025adf2840f2fc1a4ff3e74c3ddf
tatamiii <sousuke2828@gmail.com>
2020-08-18 10:24:27 +0900

    presence :tureを with optionsに

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,36 +4,30 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,
-            presence: true
+  with_options presence: true do
+
+  validates :nickname
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email,
-            presence: true,
             uniqueness: true
   # format: { with: VALID_EMAIL_REGEX }
 
   validates :password,
-            presence: true,
             format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
             length: { minimum: 6 }
 
   validates :first_name,
-            presence: true,
             format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
   validates :last_name,
-            presence: true,
             format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
   validates :first_name_kana,
-            presence: true,
             format: { with: /\A[ァ-ヶー－]+\z/ }
 
   validates :last_name_kana,
-            presence: true,
             format: { with: /\A[ァ-ヶー－]+\z/ }
 
   validates :birth_date,
-            presence: true
 end
