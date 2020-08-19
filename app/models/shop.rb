d0b1247997515bb2a623d0d8489a3c727c490a87
tatamiii <sousuke2828@gmail.com>
2020-08-19 12:06:36 +0900

    出品のバリデーションの編集

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,10 +1,22 @@
 class Shop < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category,:condition,:delivery_time,:fee,:sending_area
+  belongs_to_active_hash :category,:condition,:delivery_time,:fee_id,:sending_area
 
-  #空の投稿を保存できないようにする
-  validates :title, :text, :genre, presence: true
+  with_options presence: true do
 
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :genre_id, numericality: { other_than: 1 } 
+    validates :item_image
+    validates :item_name
+    validates :item_info
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :fee_id
+      validates :sending_area_id
+      validates :delivery_time_id
+    end
+
+    validates :item_price
+  end
+
 end
