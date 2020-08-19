35b15b8b425eaf77ea1a8512376cf22b5973c980
tatamiii <sousuke2828@gmail.com>
2020-08-19 11:55:31 +0900

    shopsコントローラーの編集

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,5 +3,22 @@ class ShopsController < ApplicationController
   end
 
   def new
+    @item = Shop.new
+  end
+
+  def create
+    @item = Shop.new(shop_params)
+    if @item.save
+      redirect_to root_path    
+    else
+     render :new
+    end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:item_image,:item_name,:item_info,:category_id,:condition_id,:fee_id,:sending_area_id,:delivery_time_id,:item_price)
+      
+    end
   end
 end
