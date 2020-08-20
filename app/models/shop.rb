1cd74ffb566a66d22edeaf18bf7bafeb09402366
tatamiii <sousuke2828@gmail.com>
2020-08-20 18:34:10 +0900

    アソシエーション編集

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,10 @@
 class Shop < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :category,:condition,:delivery_time,:fee,:sending_area
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_time
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :sending_area
   has_one_attached :item_image
 
   with_options presence: true do
