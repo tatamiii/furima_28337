077d5bcfccc8cc049e68d646646060ab1234070e
tatamiii <sousuke2828@gmail.com>
2020-08-25 10:56:09 +0900

    不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # get 'articles/index'
-  # get 'articles/new'
+
   devise_for :users
   root "exhibitions#index"
-  # resources :shops
   resources :exhibitions
 end
