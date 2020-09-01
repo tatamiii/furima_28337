a28395a3dbaa1e26e612ab51db3431b16e8d2c51
tatamiii <sousuke2828@gmail.com>
2020-09-01 18:08:51 +0900

    不要なコメントアウトの削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,4 @@ class Order < ApplicationRecord
   belongs_to :exhibition
   belongs_to_active_hash :sending_area
 
-  # with_options presence: true do
-  #   validates :postal_code,format: {with: /\A\d{3}[-]\d{4}\z/}
-  #   validates :prefecture,numericality: { other_than: 1 }
-  #   validates :city
-  #   validates :adress
-  #   validates :phone_number,format: {with: /\A\d{11}\z/}
-  # end
-
 end
