74fc7340fd18099e335a5657fd44d40728380363
tatamiii <sousuke2828@gmail.com>
2020-09-01 18:08:26 +0900

    不要なコメントアウトの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   has_many :exhibitions
-  # has_many :order
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
